8bf0bbbd9d739c5e7dc6bf92a3add6a258406c30
erzaputra <erzaputra9110@gmail.com>
2019-08-12 12:08:21 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Desc from './Desc/Desc';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  //state - data yang disimpan ( array )
+  const [appState, setAppState ]= useState({
+    apps: [
+    { id:'1', name: 'Bootstrap', show: false },
+    { id:'2', name: 'React-Js', show: false },
+    { id:'3', name: 'Laravel', show: false } 
+    ],
+    showAll: false
+  })
+
+ //change show state handler
+  const changeShowStateHandler = (event) => {
+    let input = event.target.value;
+    if (input == 1) {
+      setAppState({
+        apps: [
+        { id:'1', name: 'Bootstrap', show: true },
+        { id:'2', name: 'React-Js', show: false },
+        { id:'3', name: 'Laravel', show: false } 
+        ]
+      })
+    } else if ( input == 2){
+      setAppState({
+       apps: [
+       { id:'1', name: 'Bootstrap', show: false },
+       { id:'2', name: 'React-Js', show: true },
+       { id:'3', name: 'Laravel', show: false } 
+       ]
+     })
+    } else if ( input == 3) {
+      setAppState({
+        apps: [
+        { id:'1', name: 'Bootstrap', show: false },
+        { id:'2', name: 'React-Js', show: false },
+        { id:'3', name: 'Laravel', show: true } 
+        ]
+      })  
+    } else { 
+
+      if (appState.showAll) {
+       setAppState({
+        apps: [
+        { id:'1', name: 'Bootstrap', show: false },
+        { id:'2', name: 'React-Js', show: false },
+        { id:'3', name: 'Laravel', show: false } 
+        ],
+        showAll : false
+      })
+
+     }else {
+       setAppState({
+        apps: [
+        { id:'1', name: 'Bootstrap', show: true },
+        { id:'2', name: 'React-Js', show: true },
+        { id:'3', name: 'Laravel', show: true } 
+        ],
+        showAll : true
+      })
+     }
+
+   }
+ }
+
+ //list 
+  let nameLists = (
+    <div>
+    {appState.apps.map((nameList, index) => {
+      if (nameList.show) {
+        return <Desc 
+        delete={() => deleteStateHandler(index)}
+        apps={nameList.name} 
+        key={nameList.id}
+        rename={(event) => renameStateHandler(event,nameList.id)}
+        />
+      } 
+    })} 
     </div>
   );
+
+
+  //delete handler
+  const deleteStateHandler = (index) => {
+    const tempState = [...appState.apps]; //save current state
+    tempState.splice(index, 1); //hapus index
+    setAppState({
+      apps:tempState
+    }); 
+  }
+
+  const renameStateHandler = (event, id) => {
+    //get index
+    const appIndex = appState.apps.findIndex(ai => {
+      return ai.id === id;
+    });
+
+    //get row data of index
+    const appList = {
+      ...appState.apps[appIndex]
+    };
+
+    //rename it
+    appList.name = event.target.value;
+
+    //fetch into old state
+    const appLists = [...appState.apps];
+    appLists[appIndex] = appList;
+
+    //fetch into old state
+    setAppState({
+      apps:appLists
+    });
+
+  }
+
+//returning jsx
+ return (
+  <div className="App">
+    <h1>React-App Project</h1>
+    <div className="baris">
+      <select className="form-control" onChange={changeShowStateHandler}>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      </select>
+      
+      <button className="btn btn-primary" onClick={changeShowStateHandler} value="all">Show All !</button>
+    </div>
+    {nameLists}
+  </div>
+  );
+
 }
 
 export default App;
---
+++ src/Desc/Desc.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Desc.css'
+
+const desc = (props) => {
+	return (
+		<div className="desc-container">
+			<p>This Webapp made from {props.apps}</p>
+			<input type="text" value={props.apps} onChange={props.rename}/>
+			<button className="btn btn-danger" onClick={props.delete}>Delete</button>
+		</div>	
+		);
+
+}	
+
+export default desc
